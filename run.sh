cbacad404f777670e5ad9e526eea191bbb8e9420
Rooster <nguyenthoai06011990@gmail.com>
2025-01-11 23:00:33 +0700

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
-docker build -t metafi/enteral-kingdom-landing-page:0.1.1  . 
+# docker build -t metafi/enteral-kingdom-landing-page:0.1.1  . 
 
 
-docker buildx build --platform linux/amd64,linux/arm64 -t metafi/enteral-kingdom-landing-page:0.1.1 --push .
+# docker buildx build --platform linux/amd64,linux/arm64 -t metafi/enteral-kingdom-landing-page:0.1.1 --push .
 
 
+# docker push metafi/enteral-kingdom-landing-page:0.1.1
+docker build -t metafi/enteral-kingdom-landing-page:0.1.1 .
 docker push metafi/enteral-kingdom-landing-page:0.1.1
